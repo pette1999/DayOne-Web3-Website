8614ba7156353a2f63cdd30524c7aecf1f609e31
pette1999 <peterchen.eth@gmail.com>
2022-09-28 17:35:31 -0400

    fix logo

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -2,14 +2,16 @@ import React from 'react';
 import certik from '../assets/img/company/certik.svg';
 import google from '../assets/img/company/google.svg';
 import aptos from '../assets/img/company/aptos.svg';
-import pontem from '../assets/img/company/pontem.png';
+import meta from '../assets/img/company/meta.png';
+import pontem_white from '../assets/img/company/pontem-white.png';
+import nansen from '../assets/img/company/nansen.svg';
 import colorSharpRight from "../assets/img/color-sharp2.png"
 
 export const Brands = () => (
   <div className="brand">
     <img className="background-image-right" src={colorSharpRight} alt="Image" />
     <div>
-      <img src={pontem} alt="pontem logo"/>
+      <img src={pontem_white} alt="pontem logo"/>
     </div>
     <div>
       <img src={aptos} alt="aptos logo"/>
@@ -18,7 +20,13 @@ export const Brands = () => (
       <img src={certik} alt="certik logo"/>
     </div>
     <div>
+      <img src={nansen} alt="stanford logo"/>
+    </div>
+    <div>
       <img src={google} alt="google logo"/>
+    </div>
+    <div>
+      <img src={meta} alt="meta logo"/>
     </div>
   </div>
 );
