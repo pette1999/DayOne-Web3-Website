2a141fd4f3751f7f21131248cfbff1a4c090560a
pette1999 <peterchen.eth@gmail.com>
2022-09-28 17:00:21 -0400

    fix the newsletter

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -3,9 +3,26 @@ import { Col, Row, Alert } from "react-bootstrap";
 
 export const Newsletter = ({ status, message, onValidated }) => {
   const [email, setEmail] = useState('');
+  const [isDesktop, setDesktop] = useState(false);
 
   useEffect(() => {
     if (status === 'success') clearFields();
+
+    if (window.innerWidth > 800) {
+      setDesktop(true);
+    } else {
+      setDesktop(false);
+    }
+
+    const updateMedia = () => {
+      if (window.innerWidth > 800) {
+        setDesktop(true);
+      } else {
+        setDesktop(false);
+      }
+    };
+    window.addEventListener('resize', updateMedia);
+    return () => window.removeEventListener('resize', updateMedia);
   }, [status])
 
   const handleSubmit = (e) => {
@@ -22,25 +39,48 @@ export const Newsletter = ({ status, message, onValidated }) => {
   }
 
   return (
-      <Col lg={12}>
-        <div className="newsletter-bx wow slideInUp">
-          <Row>
-            <Col xl={6} lg={12} md={12} xs={40}>
-              <h3>Subscribe to our Newsletter<br></br> & Never miss latest updates</h3>
-              {status === 'sending' && <Alert>Sending...</Alert>}
-              {status === 'error' && <Alert variant="danger">{message}</Alert>}
-              {status === 'success' && <Alert variant="success">{message}</Alert>}
-            </Col>
-            <Col xl={6} lg={12} md={12} xs={40}>
-              <form onSubmit={handleSubmit}>
-                <div className="new-email-bx">
-                  <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" />
-                  <button type="submit">Submit</button>
-                </div>
-              </form>
-            </Col>
-          </Row>
-        </div>
+      <Col lg={15} md={12} xs={40}>
+        { 
+          isDesktop ? (
+            <div className="newsletter-bx wow slideInUp">
+              <Row>
+                <Col xl={6} lg={12} md={12} xs={40}>
+                  <h3>Subscribe to our Newsletter<br></br> & Never miss latest updates</h3>
+                  {status === 'sending' && <Alert>Sending...</Alert>}
+                  {status === 'error' && <Alert variant="danger">{message}</Alert>}
+                  {status === 'success' && <Alert variant="success">{message}</Alert>}
+                </Col>
+                <Col xl={6} lg={12} md={12} xs={40}>
+                  <form onSubmit={handleSubmit}>
+                    <div className="new-email-bx">
+                      <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" />
+                      <button type="submit">Submit</button>
+                    </div>
+                  </form>
+                </Col>
+              </Row>
+            </div>
+          ) : (
+            <div className="newsletter-s">
+              <Row>
+                <Col xl={6} lg={12} md={12} xs={40}>
+                  <h3>Subscribe to our Newsletter & Never miss latest updates</h3>
+                  {status === 'sending' && <Alert>Sending...</Alert>}
+                  {status === 'error' && <Alert variant="danger">{message}</Alert>}
+                  {status === 'success' && <Alert variant="success">{message}</Alert>}
+                </Col>
+                <Col xl={6} lg={12} md={12} xs={40}>
+                  <form onSubmit={handleSubmit}>
+                    <div className="new-email-s">
+                      <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" />
+                      <button type="submit">Submit</button>
+                    </div>
+                  </form>
+                </Col>
+              </Row>
+            </div>
+          )
+        }
       </Col>
   )
 }
