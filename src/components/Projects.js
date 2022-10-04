3d8df46c79131039511f3564a5d43b1d9b47f2d1
pette1999 <peterchen.eth@gmail.com>
2022-10-04 14:18:52 -0700

    update content

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -30,7 +30,7 @@ export const Projects = () => {
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                     <Tab.Pane eventKey="first">
                       <h3>Challenge Kick off Events</h3>
-                      <p><b>Time:</b> Oct 14, 2022 6pm - 7pm<br></br><b>Location:</b> Huang Basement Auditorium (475 Via Ortega, Stanford, CA 94305)<br></br></p>
+                      <p><b>Time:</b> Oct 14, 2022 6pm - 7pm<br></br><b>Location:</b> TBD<br></br></p>
                       <p>Topics include Introduction of the Move Student Challenge, Introduction of Aptos (pre-launch or post-launch based on the actual launch date), Prerequisite of Move Student Challenge - as long as you are eager to learn Web 3.0! etc.</p>
                       <h4>Join the waitlist, Details Coming Soon...</h4>
                     </Tab.Pane>
@@ -42,7 +42,7 @@ export const Projects = () => {
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
                       <h3>Onsite Challenge</h3>
-                      <p><b>Time:</b> Jan 14, 2023(Saturday) - Jan 16, 2023(Monday/Public Holiday)<br></br><b>Location:</b> Huang Basement Auditorium (475 Via Ortega, Stanford, CA 94305)</p>
+                      <p><b>Time:</b> Jan 14, 2023(Saturday) - Jan 16, 2023(Monday/Public Holiday)<br></br><b>Location:</b> TBD</p>
                       <h4>Join the waitlist, Details Coming Soon...</h4>
                     </Tab.Pane>
                   </Tab.Content>
