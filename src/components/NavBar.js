7f91bc85e2a44b631db687dae0a0b51c554b422d
peter <peterchen.eth@gmail.com>
2022-10-27 19:09:05 +0000

    Update 3 files

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -46,6 +46,7 @@ export const NavBar = () => {
               <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
               <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
               <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Move Challenge</Nav.Link>
+              <Nav.Link href="/move_challenge" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Challenge</Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
